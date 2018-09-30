8dcbdd331df7efc0fe2ae9127ab620780a93f264
Lean Sibal <lsibal@apdgroup.com>
2018-09-30 19:57:09 +0800

    Plugin frame, custom taxonomy and custom taxonomy group

--- plugin.php
+++ plugin.php
@@ -11,3 +11,81 @@
  */
 
 if( ! defined(  'ABSPATH' ) ) exit;
+
+class WC_Ingredient_Sensitivity {
+	private static $instance;
+
+	private static $actions = [
+		'register_ingredients_taxonomy' => 'init',
+		'ingredient_groups_submenu_page' => 'admin_menu'
+	];
+
+	public static function get_instance() {
+		if( self:: $instance === null ) {
+			self::$instance = new self();
+		}
+		return self::$instance;
+	}
+
+	public function __construct() {
+		$this->setup_filters();
+		$this->setup_actions();
+	}
+
+	protected function setup_filters() {
+	}
+
+	protected function setup_actions() {
+		foreach( self::$actions as $function => $hook ) {
+			add_action( $hook, [ $this, $function ] );
+		}
+	}
+
+	public function register_ingredients_taxonomy() {
+		$labels = [
+			'name'              => _x( 'Ingredients', 'taxonomy general name', 'woocommerce-ingredient-sensitivity' ),
+			'singular_name'     => _x( 'Ingredient', 'taxonomy singular name', 'woocommerce-ingredient-sensitivity' ),
+			'search_items'      => __( 'Search Ingredients', 'woocommerce-ingredient-sensitivity' ),
+			'all_items'         => __( 'All Ingredients', 'woocommerce-ingredient-sensitivity' ),
+			'parent_item'       => __( 'Parent Ingredient', 'woocommerce-ingredient-sensitivity' ),
+			'parent_item_colon' => __( 'Parent Ingredient:', 'woocommerce-ingredient-sensitivity' ),
+			'edit_item'         => __( 'Edit Ingredient', 'woocommerce-ingredient-sensitivity' ),
+			'update_item'       => __( 'Update Ingredient', 'woocommerce-ingredient-sensitivity' ),
+			'add_new_item'      => __( 'Add New Ingredient', 'woocommerce-ingredient-sensitivity' ),
+			'new_item_name'     => __( 'New Ingredient Name', 'woocommerce-ingredient-sensitivity' ),
+			'menu_name'         => __( 'Ingredients', 'woocommerce-ingredient-sensitivity' ),
+		];
+		register_taxonomy(
+			'ingredients',
+			[ 'product' ], [
+				'hierarchical' => true,
+				'labels' => $labels,
+				'rewrite' => true,
+				'query_var' => true
+			]
+		);
+	}
+
+	public function ingredient_groups_submenu_page() {
+		add_submenu_page(
+			'edit.php?post_type=product',
+			'Ingredient Groups',
+			'Ingredient Groups',
+			'manage_options',
+			'ingredient_groups_page',
+			[ &$this, 'ingredient_groups_page' ]
+		);
+	}
+
+	public function ingredient_groups_page() {
+		ob_start();
+		echo "hello";
+		echo ob_get_clean();
+	}
+
+}
+
+
+add_action('plugins_loaded', function(){
+	WC_Ingredient_Sensitivity::get_instance();
+});
