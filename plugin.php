c3cdcbae060294af83e5a88c052b9c6986de837e
Lean Sibal <lsibal@apdgroup.com>
2018-09-30 09:46:26 +0800

    Plugin properties

---
+++ plugin.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Plugin Name: WooCommerce Ingredient Sentivity
+ * Plugin URI: https://github.com/LeanSibal/woocommerce-ingredient-sensitivity
+ * Description: Allows Customers to select ingredients and ingredient group sentitivity based on products
+ * Author: Lean Sibal
+ * Author URI: https://github.com/LeanSibal/
+ * Version: 0.0.1
+ * Text Domain: woocommerce-ingredient-sensitivity
+ * Domain Path: /languages
+ */
+
+if( ! defined(  'ABSPATH' ) ) exit;
